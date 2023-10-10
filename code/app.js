69d630801849883cfed953c3494a967985cacf89
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-10-10 15:00:38 +0100

    added input, lineWidth ranger

--- code/app.js
+++ code/app.js
@@ -1,8 +1,9 @@
+const lineWidth = document.getElementById('line-width')
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 canvas.width = 800
 canvas.height = 800
-ctx.lineWidth = 2
+ctx.lineWidth = lineWidth.value
 let isPainting = false
 
 function onMove(event) {
@@ -11,6 +12,8 @@ function onMove(event) {
     ctx.stroke()
     return
   }
+  // To make all lines seperated depends on width and colours
+  ctx.beginPath()
   ctx.moveTo(event.offsetX, event.offsetY)
 }
 
@@ -21,7 +24,15 @@ function cancelPainting() {
   isPainting = false
 }
 
+function onLineWidthChange(event) {
+  //Updated the ctx with new value
+  ctx.lineWidth = event.target.value
+}
+
+// All the user actions has to be an event
 canvas.addEventListener('mousemove', onMove)
 canvas.addEventListener('mousedown', startPainting)
 canvas.addEventListener('mouseup', cancelPainting)
 canvas.addEventListener('mouseleave', cancelPainting)
+
+lineWidth.addEventListener('change', onLineWidthChange)
