9d07c87cf2f0735070d8d058c19a47f2254f4fe8
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-10-10 17:05:14 +0100

    filling mode

--- code/app.js
+++ code/app.js
@@ -1,11 +1,16 @@
-const lineWidth = document.getElementById('line-width')
+const modeBtn = document.getElementById('mode-btn')
+const colorOptions = Array.from(
+  document.getElementsByClassName('color-option')
+)
 const color = document.getElementById('color')
+const lineWidth = document.getElementById('line-width')
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 canvas.width = 800
 canvas.height = 800
 ctx.lineWidth = lineWidth.value
 let isPainting = false
+let isFilling = false
 
 function onMove(event) {
   if (isPainting) {
@@ -35,11 +40,39 @@ function onColorChange(event) {
   ctx.fillStyle = event.target.value
 }
 
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color
+  ctx.strokeStyle = colorValue
+  ctx.fillStyle = colorValue
+  color.value = colorValue
+}
+
+function onModeBtn() {
+  if (isFilling) {
+    isFilling = false
+    modeBtn.innerText = 'Fill'
+  } else {
+    isFilling = true
+    modeBtn.innerText = 'Draw'
+  }
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, 800, 800)
+  }
+}
+
 // All the user actions has to be an event
 canvas.addEventListener('mousemove', onMove)
 canvas.addEventListener('mousedown', startPainting)
 canvas.addEventListener('mouseup', cancelPainting)
 canvas.addEventListener('mouseleave', cancelPainting)
+canvas.addEventListener('click', onCanvasClick)
 
 lineWidth.addEventListener('change', onLineWidthChange)
 color.addEventListener('change', onColorChange)
+
+
+colorOptions.forEach(color => color.addEventListener('click', onColorClick))
+modeBtn.addEventListener('click', onModeBtn)
